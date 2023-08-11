a693ba111d8ebbb2af0c4c7e8901195a13d4f0f2
Miao Sang <118147601+Sarahsang@users.noreply.github.com>
2023-08-11 15:57:33 +1200

    Update 642Assignment 1Miao Sang .py

--- 642Assignment 1Miao Sang .py
+++ 642Assignment 1Miao Sang .py
@@ -6,7 +6,7 @@ class GroupExercise:
         self.__trainer = None
         self.__max_capacity = max_capacity
         self.__participants = []
-        self.__waitlist = []
+        self.__wait_list = []
         self.__fee_amount = fee_amount
         self.__checked_in = []
     
@@ -28,8 +28,8 @@ class GroupExercise:
     def get_participants(self):
         return self.__participants
     
-    def get_waitlist(self):
-        return self.__waitlist
+    def get_wait_list(self):
+        return self.__wait_list
     
     def get_fee_amount(self):
         return self.__fee_amount
@@ -47,15 +47,15 @@ class GroupExercise:
             self.__participants.append(member)
             member.add_group_exercise(self)
         else:
-            self.__waitlist.append(member)
+            self.__wait_list.append(member)
     
     #remove member        
     def remove_member(self, member):
         if member in self.__participants:
             self.__participants.remove(member)
             member.remove_group_exercise(self)
-        elif member in self.__waitlist:
-            self.__waitlist.remove(member)
+        elif member in self.__wait_list:
+            self.__wait_list.remove(member)
         else:
             print("Member not found")
     
@@ -84,6 +84,23 @@ class GroupExercise:
     #attendance
     def attendance(self):
         return len(self.__checked_in)
+    
+    #mark attendance
+    def mark_attendance(self, member):
+        if member in self.__participants:
+            if member in self.__checked_in:
+                print("Member already checked in")
+            else:
+                self.__checked_in.append(member)
+        else:
+            print("Member not enrolled in this group exercise")
+    
+    #attendance rate
+    def attendance_rate(self):
+        return len(self.__checked_in) / len(self.__participants)
+
+    def __str__(self):
+        return f"GroupExercise(Name: {self.__name}, Trainer: {self.__trainer.get_name() if self.__trainer else 'None'}, Capacity: {self.__max_capacity})"
 
 # create member class
 
@@ -103,8 +120,27 @@ class Member:
         return self.__enrolled_group_exercise
     
     # methods for Member
+    #book group exercise
+    def book_group_exercise(self, group_exercise):
+        if group_exercise in self.__enrolled_group_exercise:
+            print("You are already enrolled in this group exercise")
+        else:
+            group_exercise.enroll(self)
     
-    
+    #cancel group exercise
+    def cancel_group_exercise(self, group_exercise):
+        if group_exercise in self.__enrolled_group_exercise:
+            group_exercise.remove_member(self)
+        else:
+            print("You are not enrolled in this group exercise")
+            
+    #display booked group exercise
+    def booked_group_exercise(self):
+        for group_exercise in self.__enrolled_group_exercise:
+            print(group_exercise.get_name())
+            
+    def __str__(self):
+        return f"Member(Name: {self.__full_name}, Membership No: {self.__id_number})"
     
 #create trainer class
 class Trainer:
@@ -122,4 +158,41 @@ class Trainer:
         return self.__assigned_group_exercises
 
     # methods for Trainer
+    #display assigned group exercise
+    def assigned_group_exercises(self):
+        for group_exercise in self.__assigned_group_exercises:
+            print(group_exercise.get_name())
+            
+    #add group exercise
+    def add_group_exercise(self, group_exercise):
+        if group_exercise in self.__assigned_group_exercises:
+            print("This group exercise is already assigned to this trainer")
+        else:
+            self.__assigned_group_exercises.append(group_exercise)
+            
+    def __str__(self):
+        return f"Trainer(Name: {self.__full_name}, Specialisation: {self.__specialisation})"
     
+    
+#create test data for group exercise
+
+groupexercise1 = GroupExercise("Yoga", 10, 10)
+groupexercise2 = GroupExercise("Zumba", 20, 20)
+
+print(groupexercise1)
+print(groupexercise2)
+
+#create test data for members
+member1 = Member("Miao Sang", 1)
+member2 = Member("Oliver Green", 2)
+member3 = Member("John Smith", 3)
+member4 = Member("Jane Doe", 4)
+member5 = Member("Mary Jane", 5)
+
+print(member1)
+
+#create test data for trainers
+trainer1 = Trainer("John Armstrong", "Yoga")
+trainer2 = Trainer("peter Parker", "Zumba")
+
+print(trainer1)
